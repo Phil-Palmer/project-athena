81134a548b13985cc5c7523278d29411ac5dd945
Phil Palmer <35943148+Phil-Palmer@users.noreply.github.com>
2020-04-01 21:44:05 -0400

    safe

--- libraries/render-utils/src/Model.h
+++ libraries/render-utils/src/Model.h
@@ -166,7 +166,6 @@ public:
     void reset();
 
     void setSnapModelToRegistrationPoint(bool snapModelToRegistrationPoint, const glm::vec3& registrationPoint);
-    bool getSnapModelToRegistrationPoint() { return _snapModelToRegistrationPoint; }
 
     virtual void simulate(float deltaTime, bool fullUpdate = true);
     virtual void updateClusterMatrices();
@@ -214,9 +213,6 @@ public:
     void setSnapModelToCenter(bool snapModelToCenter) {
         setSnapModelToRegistrationPoint(snapModelToCenter, glm::vec3(0.5f,0.5f,0.5f));
     };
-    bool getSnapModelToCenter() {
-        return _snapModelToRegistrationPoint && _registrationPoint == glm::vec3(0.5f,0.5f,0.5f);
-    }
 
     /// Returns the number of joint states in the model.
     int getJointStateCount() const { return (int)_rig.getJointStateCount(); }
